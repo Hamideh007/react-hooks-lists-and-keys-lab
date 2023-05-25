9daab408d9afebf70e97acf18c39f51125e26870
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-05-25 01:04:01 -0400

    keys-lab complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,14 +2,17 @@ import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
   return (
-    <div className="project-item">
+    <div>
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div>
+        {technologies.map((technology, index) => (
+          <span key={index}>{technology}</span>
+        ))}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
